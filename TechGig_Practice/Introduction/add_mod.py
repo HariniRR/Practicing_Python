f19cdad77ade7f5abaf67a1d2dc07d026a70638c
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-07-08 12:21:06 +0530

    Add files via upload

---
+++ TechGig_Practice/Introduction/add_mod.py
@@ -0,0 +1,19 @@
+//You just need to take two numbers as input from stdin and you need to find modulus of them.
+
+def main():
+    a=int(input())
+    b=int(input())
+    print(a%b)
+
+ # Write code here 
+
+main()
+
+// addition
+def main():
+    a=int(input())
+    b=int(input())
+    print(a+b)
+ # Write code here 
+
+main()
---
+++ TechGig_Practice/Introduction/mul_div.py
@@ -0,0 +1,17 @@
+#You just need to take two numbers as input from stdin and you need to divide them.
+def main():
+    a=int(input())
+    b=int(input())
+    print(a//b)
+ # Write code here 
+
+main()
+
+#You just need to take two numbers as input from stdin and you need to multiply them.
+def main():
+    a=int(input())
+    b=int(input())
+    print(a*b)
+ # Write code here 
+
+main()
