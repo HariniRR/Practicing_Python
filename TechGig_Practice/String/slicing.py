2e034e311291ef82bb8f06eaa745d2f081e15032
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-07-08 12:28:47 +0530

    Add files via upload

---
+++ TechGig_Practice/String/slicing.py
@@ -0,0 +1,10 @@
+#You need to take string input and two other numbers which will be the start and end point of the slice and you need to print that slice of string to the stdout.
+
+def main():
+ stri=input()
+ st=int(input())
+ end=int(input())+1
+ print(stri[st:end])
+ # Write code here 
+
+main()
