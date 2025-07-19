f2de02aed7981b5b3637b3f4940de329bb8ae2f5
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-07-19 15:19:59 +0530

    Add files via upload

---
+++ TechGig_Practice/String/FirstLetter.py
@@ -0,0 +1,16 @@
+```Input Format
+You will be taking a string as an input from stdin.
+
+Constraints
+1 <= |S| <= 100000
+
+Output Format
+You need to print the first character of the string to the stdout.```
+
+ #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    s = input()
+    print(s[0])
+
+main()
